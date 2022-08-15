f873051d022de3bf8c429294af28bdab48600a7e
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-15 15:59:46 +0600

    Day 1 Let and Const

---
+++ 10 Days of Javascript/day-1-let-and-const/index.md
@@ -0,0 +1,16 @@
+---
+title:  Day 1 Let and Const
+ref: https://www.hackerrank.com/challenges/js10-let-and-const/problem?isFullScreen=true
+---
+## [Day 1 Let and Const](https://www.hackerrank.com/challenges/js10-let-and-const/problem?isFullScreen=true)
+
+ ```js
+    const PI = Math.PI
+    const r = readLine() // 2.6
+    var area = parseFloat(PI * r * r)
+    console.log(area)
+
+    var peri = parseFloat(2 * PI * r)
+    console.log(peri)
+  ```
+  
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-function factorial(n) {
-	if (n === 1) {
-		return 1
-	}
-	return n * factorial(n - 1)
-}
+const PI = Math.PI
+const r = readLine() // 2.6
+var area = parseFloat(PI * r * r)
+console.log(area)
 
-factorial(5) // 120
-
-// Explanation:
-// The function is recursive. It calls itself until it reaches the base case. In this case, n === 1, which returns 1. n * factorial(n - 1) is the recursive call. Since n - 1 === 0, it returns 1. n * factorial(n - 1) === n * 1 === n.
+var peri = parseFloat(2 * PI * r)
+console.log(peri)
