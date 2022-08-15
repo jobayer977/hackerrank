16f7e37eab9945c8af5a443fb6096569ccec62ae
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-15 15:30:00 +0600

    Day 3 Throw

---
+++ 10 Days of Javascript/day-3-throw/index.md
@@ -0,0 +1,20 @@
+---
+title:  Day 3 Throw
+ref: https://www.hackerrank.com/challenges/js10-throw/problem?isFullScreen=true
+---
+## [Day 3 Throw](https://www.hackerrank.com/challenges/js10-throw/problem?isFullScreen=true)
+
+ ```js
+   function isPositive(a) {
+    if (a > 0) {
+      return 'YES'
+    } else if (a === 0) {
+      throw new Error('Zero Error')
+    } else {
+      throw new Error('Negative Error')
+    }
+  }
+  ```
+  
+**Explanation**
+The if statement checks if 'a' is greater than 0. If it is, the function returns 'YES'. If 'a' is 0, the function throws an error with the message 'Zero Error'. If 'a' is less than 0, the function throws an error with the message 'Negative Error'. The else statement is not needed because the function will never reach the else statement if 'a' is 0 or greater than 0.
--- index.js
+++ index.js
@@ -16,18 +16,3 @@ function isPositive(a) {
 
 // Explanation
 // The if statement checks if 'a' is greater than 0. If it is, the function returns 'YES'. If 'a' is 0, the function throws an error with the message 'Zero Error'. If 'a' is less than 0, the function throws an error with the message 'Negative Error'. The else statement is not needed because the function will never reach the else statement if 'a' is 0 or greater than 0.
-
-/**
- *   A line of code that prints "Hello, World!" on a new line is provided in the editor.
- *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
- *
- *	Parameter:
- *   parameterVariable - A string of text.
- **/
-function greeting(parameterVariable) {
-	console.log('Hello, World!')
-	console.log(parameterVariable)
-}
-
-// Explanation
-// The first line of code prints 'Hello, World!' to the console. The second line of code prints the value of 'parameterVariable' to the console. The parameterVariable is a string of text.
