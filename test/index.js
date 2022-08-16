85082e2e36e4e4ce276e93f0057d4fcbe4f22b13
Jobayerdev <jobayerhossain977@gmail.com>
2022-08-16 11:31:32 +0600

    docs(Add Topic): :fire: Day 5 Arrow Functions

--- test/index.js
+++ test/index.js
@@ -1,20 +1,12 @@
-class Rectangle {
-	constructor(w, h) {
-		this.w = w
-		this.h = h
+function modifyArray(nums) {
+	for (const n in nums) {
+		if (nums[n] % 2 === 0) {
+			nums[n] = nums[n] * 2
+		} else if (nums[n] % 2 === 1) {
+			nums[n] = nums[n] * 3
+		}
 	}
+	return nums
 }
 
-const methods = {
-	area: function () {
-		return this.w * this.h
-	},
-}
-
-Object.assign(Rectangle.prototype, methods)
-
-class Square extends Rectangle {
-	constructor(w) {
-		super(w, w)
-	}
-}
+console.log(modifyArray([1, 2, 3, 4, 5]))
